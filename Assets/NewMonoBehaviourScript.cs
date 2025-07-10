91e81741913dfb8456571c17fc0707681b123dd7
DESKTOP-GIUQICH\202_Main <jungchanjoong@gmail.com>
2025-07-10 11:29:02 +0900

    a=1

--- Assets/NewMonoBehaviourScript.cs
+++ Assets/NewMonoBehaviourScript.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+    int a = 1;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
